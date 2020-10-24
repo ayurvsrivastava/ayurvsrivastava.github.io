3a0afa7c0fbd92772975b38a7c2c69985bf70bdc
ayurvsrivastava <73374023+ayurvsrivastava@users.noreply.github.com>
2020-10-23 23:15:45 -0400

    Create calculateGrade.js

---
+++ assets/js/calculateGrade.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+function calculateGrade() {
+	var table = document.getElementById("datatable");
+	var length = table.rows.length;
+	var i;
+	var t;
+	var count = 0;
+	var emptyGrade;
+	var emptyWeight;
+	var weightScore = 0;
+	var totalWeight = 0;
+	var totalGrade = 0;
+	for(t = 1; t < length; t++) {
+		emptyGrade += document.getElementById("grade"+t).value;
+		emptyWeight += document.getElementById("weight"+t).value;
+	}
+	for (var z = 1; z < length; z++) {
+		if (document.getElementById("grade"+z).value !="" && document.getElementById("weight"+z).value != ""){
+			count += document.getElementById("weight"+z).value*1;
+		}
+	}
+	if (emptyGrade == 'undefined' && emptyWeight == 'undefined') {
+		alert("Fill in Values into the table");
+		return;
+	}
+	for (i = 1; i < length; i++) {
+		if (document.getElementById("weight" + i).value < 0 || document.getElementById("grade" + i).value < 0) {
+			alert("Weight and Grade Values Cannot be Negative");
+			return;
+		}
+		weightScore += document.getElementById("grade"+i).value*document.getElementById("weight"+i).value;
+		totalWeight = count;
+		totalGrade = weightScore/totalWeight;
+	}
+	totalGrade=totalGrade.toFixed(2);
+	document.getElementById("result").innerHTML = "Current Grade in Class is " + totalGrade.toString() +"%";
+}
